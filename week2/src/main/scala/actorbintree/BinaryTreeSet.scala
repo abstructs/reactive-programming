fb9965f4ff8faf523c8f240d0d997485fa7e43da
abstructs <andrewclarke540@gmail.com>
2019-08-31 18:10:20 -0400

    insert, contains and remove working, add task managment queue to syncronize events

--- week2/src/main/scala/actorbintree/BinaryTreeSet.scala
+++ week2/src/main/scala/actorbintree/BinaryTreeSet.scala
@@ -10,9 +10,19 @@ object BinaryTreeSet {
 
   def main(args: Array[String]): Unit = {
     val system = ActorSystem("test")
-    val actor = system.actorOf(Props(classOf[BinaryTreeSet]))
+    val client = system.actorOf(Props(classOf[BinaryTreeSet]))
 
-    actor.tell(Insert(actor, 1, 1), actor)
+    client.tell(Insert(client, id=100, elem=1), client)
+    client.tell(Contains(client, id=50, elem=2), client)
+    client.tell(Contains(client, id=60, elem=1), client)
+    client.tell(Remove(client, id=70, elem=1), client)
+    client.tell(Contains(client, id=80, elem=1), client)
+    client.tell(Insert(client, id=90, elem=1), client)
+    client.tell(Contains(client, id=110, elem=1), client)
+    client.tell(Remove(client, id=120, elem=1), client)
+    client.tell(Insert(client, id=130, elem=2), client)
+    client.tell(Contains(client, id=140, elem=1), client)
+    client.tell(Contains(client, id=150, elem=2), client)
   }
 
   trait Operation {
@@ -54,7 +64,6 @@ object BinaryTreeSet {
   
   /** Message to signal successful completion of an insert or remove operation. */
   case class OperationFinished(id: Int) extends OperationReply
-
 }
 
 class BinaryTreeSet extends Actor {
@@ -69,14 +78,42 @@ class BinaryTreeSet extends Actor {
   var pendingQueue = Queue.empty[Operation]
 
   // optional
-  def receive = {
-    case Insert(req, id, elem) => println(id)
-    case _ => println("invalid operation")
-  }
+  def receive= normal
 
   // optional
   /** Accepts `Operation` and `GC` messages. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case OperationFinished(id) => onOperationFinished(OperationFinished(id))
+    case ContainsResult(id, res) => onOperationFinished(ContainsResult(id, res))
+    case op: Operation => handleOperation(op)
+  }
+
+  private def handleOperation(op: Operation): Unit = {
+    if(pendingQueue.isEmpty) {
+      root.tell(op, op.requester)
+    } else {
+      pendingQueue = pendingQueue.enqueue(op)
+    }
+  }
+
+  private def printReply: (OperationReply) => Unit = {
+    case OperationFinished(id) => printf("Operation finished: { id: %s }\n", id)
+    case ContainsResult(id, res) => printf("Contains result: { id: %s, response: %s }\n", id, res)
+  }
+
+  private def processNext: Unit = {
+    if (!pendingQueue.isEmpty) {
+      val (nextOp, newQueue) = pendingQueue.dequeue
+      receive(nextOp)
+
+      pendingQueue = newQueue
+    }
+  }
+
+  private def onOperationFinished(op: OperationReply): Unit = {
+    printReply(op)
+    processNext
+  }
 
   // optional
   /** Handles messages while garbage collection is performed.
@@ -106,12 +143,62 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
   var subtrees = Map[Position, ActorRef]()
   var removed = initiallyRemoved
 
+  def handleInsert(op: Insert): Unit = {
+    if (op.elem == elem) {
+      if (removed) {
+        removed = false
+      }
+
+      op.requester.tell(OperationFinished(op.id), self)
+    } else if (op.elem > elem && subtrees.contains(Right)) {
+      subtrees(Right).forward(op)
+    } else if (op.elem < elem && subtrees.contains(Left)) {
+      subtrees(Left).forward(op)
+    } else {
+      val newNode = context.actorOf(BinaryTreeNode.props(op.elem, initiallyRemoved = false))
+      val position: Position = if (op.elem > elem) Right else Left
+
+      subtrees += (position -> newNode)
+
+      op.requester.tell(OperationFinished(op.id), self)
+    }
+  }
+
+  def handleContains(op: Operation): Unit = {
+    if (op.elem == elem) {
+      op.requester.tell(ContainsResult(op.id, !removed), self)
+    } else if (op.elem > elem && subtrees.contains(Right)) {
+      subtrees(Right).forward(op)
+    } else if (op.elem < elem && subtrees.contains(Left)) {
+      subtrees(Left).forward(op)
+    } else {
+      op.requester.tell(ContainsResult(op.id, false), self)
+    }
+  }
+
+  def handleRemove(op: Operation): Unit = {
+    if (op.elem == elem && !removed) {
+      removed = true
+      op.requester.tell(OperationFinished(op.id), self)
+    } else if (op.elem > elem && subtrees.contains(Right)) {
+      subtrees(Right).forward(op)
+    } else if (op.elem < elem && subtrees.contains(Left)) {
+      subtrees(Left).forward(op)
+    } else {
+      op.requester.tell(OperationFinished(op.id), self)
+    }
+  }
+
   // optional
   def receive = normal
 
   // optional
   /** Handles `Operation` messages and `CopyTo` requests. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case Insert(requester, id, newElem) => handleInsert(Insert(requester, id, newElem))
+    case Contains(requester, id, elemToFind) => handleContains(Contains(requester, id, elemToFind))
+    case Remove(requester, id, elemToRemove) => handleRemove(Remove(requester, id, elemToRemove))
+  }
 
   // optional
   /** `expected` is the set of ActorRefs whose replies we are waiting for,
